11e5ba513e9142a52e5754ff1f8c99f8befaa4c0
Ilya N. Troychenkov <mail@logicflow.ru>
2019-08-09 00:00:11 +0300

    1C Cache Cleaning 0.2.0808.2359
    + Незначительные изменения в интерфейсе

--- 1C Cache Cleaning/Properties/AssemblyInfo.cs
+++ 1C Cache Cleaning/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Logic Flow LLC.")]
 [assembly: AssemblyProduct("1C Cache Cleaning")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("Logic Flow LLC.")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0808.2250")]
-[assembly: AssemblyFileVersion("0.2.0808.2250")]
+[assembly: AssemblyVersion("0.2.0808.2359")]
+[assembly: AssemblyFileVersion("0.2.0808.2359")]
